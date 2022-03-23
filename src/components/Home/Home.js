273b937a96c91f36d3cc5670a68ab1fcddacb85e
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-23 15:28:47 -0700

    and bracket to booklist in home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default function Book() {
   return ( 
     <> 
-      <NavLink exact to='/bookList'></NavLink>
+      <NavLink exact to={'/bookList'}></NavLink>
     </>
   );
 }
