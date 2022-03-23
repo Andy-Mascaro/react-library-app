ef48080518b66a45351c69fb1dc82a5769a135be
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-23 11:44:54 -0700

    added env file for supabase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <Switch>
+          <h1>Library Catalog</h1>
+          <BookList />
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
