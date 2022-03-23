575d97a7a7625d1f39a7051354440e067c7d0c72
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-23 15:24:39 -0700

    added exact to my my navlink function

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default function Book() {
   return ( 
     <> 
-      <NavLink to='/bookList'></NavLink>
+      <NavLink exact to='/bookList'></NavLink>
     </>
   );
 }
